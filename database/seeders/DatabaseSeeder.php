81f74072b0845bfe2285f15845d82c4026715ef8
Oussama Bel-kaze <oussamaazrour@gmail.com>
2024-03-04 10:41:16 +0100

    Integration de Breeze

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $role = Role::create(['name' => 'spectator']);
+        // $role = Role::create(['name' => 'admin']);
+        // $role = Role::create(['name' => 'organizer']);
+
+
+        // \App\Models\User::factory(10)->create();
+        
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
